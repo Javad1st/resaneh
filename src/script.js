4568276cbeb97fed4d40d382b06b60a7b07953fa
Javad1st <javadooshakhlari@gmail.com>
2025-02-01 19:46:30 +0330

    tail

--- src/script.js
+++ src/script.js
@@ -25,21 +25,64 @@ document.addEventListener('click', () => {
     }
 });
 
-////////////
+///////////
 
-// انتخاب عناصر
 const hamberButton = document.getElementById('hamber');
 const hamberContent = document.getElementById('hamberContent');
 const closeButton = document.getElementById('closeButton');
 
-// رویداد کلیک برای دکمه همبرگر
+
 hamberButton.addEventListener('click', () => {
-    hamberContent.classList.remove('translate-x-full'); // حذف کلاس برای نمایش منو
-    hamberContent.classList.add('translate-x-0'); // اضافه کردن کلاس برای نمایش منو
+    hamberContent.classList.remove('translate-x-full'); 
+    hamberContent.classList.add('translate-x-0'); 
 });
 
-// رویداد کلیک برای دکمه بستن منو
+
 closeButton.addEventListener('click', () => {
-    hamberContent.classList.remove('translate-x-0'); // حذف کلاس برای پنهان کردن منو
-    hamberContent.classList.add('translate-x-full'); // اضافه کردن کلاس برای پنهان کردن منو
+    hamberContent.classList.remove('translate-x-0'); 
+    hamberContent.classList.add('translate-x-full'); 
+});
+
+
+///////
+
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
 });
